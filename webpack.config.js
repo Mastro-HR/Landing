e39926b27ca25e64d71c827ea21b739c49fa45c2
GianScala <38752338+GianScala@users.noreply.github.com>
2024-12-12 15:56:13 +0100

    Update general

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const CopyPlugin = require('copy-webpack-plugin'); // Add this line
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
     publicPath: '/',
   },
