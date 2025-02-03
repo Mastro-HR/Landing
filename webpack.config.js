3efa395ada1d8b529cf4920bef34410e0757c40b
GianScala <38752338+GianScala@users.noreply.github.com>
2025-02-03 13:28:51 +0100

    Update build configuration for Vercel

--- webpack.config.js
+++ webpack.config.js
@@ -86,14 +86,6 @@ module.exports = {
     enforceExtension: false,
     alias: {
       '@': path.resolve(__dirname, 'src'),
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@pages': path.resolve(__dirname, 'src/pages'),
-      '@assets': path.resolve(__dirname, 'src/assets'),
-      '@utils': path.resolve(__dirname, 'src/utils'),
-      '@hooks': path.resolve(__dirname, 'src/hooks'),
-      '@styles': path.resolve(__dirname, 'src/styles'),
-      '@services': path.resolve(__dirname, 'src/services'),
-      '@constants': path.resolve(__dirname, 'src/constants')
     },    
     modules: ['node_modules', path.resolve(__dirname, './src')]
   },
