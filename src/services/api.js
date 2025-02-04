40c007c0d291546c6ad92af3cf1a30ebcb50737c
Mastro-HR <cia@mastrohr.com>
2025-02-04 09:48:52 +0100

    Updated API base URL to use custom domain

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 // src/services/api.js
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = 'https://api.mastrohr.com';
 import { cleanUrl } from '@/utils/validation';
 
 export const hiringContextService = {
